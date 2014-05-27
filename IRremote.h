c1daafe10f0ee4f41528103c7f6543ed47c465c7
Xun Yang <x.yang@arduino.cc>
2014-05-27 11:13:39 +0200

    Beginning of TinkerKitIRremote

--- IRremote.h
+++ IRremote.h
@@ -55,26 +55,29 @@ class IRrecv
 {
 public:
   IRrecv(int recvpin);
+  void setRecvPin(int recvpin);
   void blink13(int blinkflag);
   int decode(decode_results *results);
   void enableIRIn();
   void resume();
 private:
   // These are called by decode
-  int getRClevel(decode_results *results, int *offset, int *used, int t1);
-  long decodeNEC(decode_results *results);
-  long decodeSony(decode_results *results);
-  long decodeSanyo(decode_results *results);
-  long decodeMitsubishi(decode_results *results);
-  long decodeRC5(decode_results *results);
-  long decodeRC6(decode_results *results);
-  long decodePanasonic(decode_results *results);
-  long decodeJVC(decode_results *results);
-  long decodeHash(decode_results *results);
+  //int getRClevel(decode_results *results, int *offset, int *used, int t1);
+  long decodeNEC(decode_results *results, bool iTK);	//Note the Tinkerkit remotes have different codec than standard NEC, see IRremote.cpp for details
+  
+  //long decodeSony(decode_results *results);
+  //long decodeSanyo(decode_results *results);
+  //long decodeMitsubishi(decode_results *results);
+  //long decodeRC5(decode_results *results);
+  //long decodeRC6(decode_results *results);
+  //long decodePanasonic(decode_results *results);
+  //long decodeJVC(decode_results *results);
+  //long decodeHash(decode_results *results);
   int compare(unsigned int oldval, unsigned int newval);
 
 } 
 ;
+
 
 // Only used for testing; can remove virtual for shorter code
 #ifdef TEST
@@ -82,30 +85,6 @@ private:
 #else
 #define VIRTUAL
 #endif
-
-class IRsend
-{
-public:
-  IRsend() {}
-  void sendNEC(unsigned long data, int nbits);
-  void sendSony(unsigned long data, int nbits);
-  // Neither Sanyo nor Mitsubishi send is implemented yet
-  //  void sendSanyo(unsigned long data, int nbits);
-  //  void sendMitsubishi(unsigned long data, int nbits);
-  void sendRaw(unsigned int buf[], int len, int hz);
-  void sendRC5(unsigned long data, int nbits);
-  void sendRC6(unsigned long data, int nbits);
-  void sendDISH(unsigned long data, int nbits);
-  void sendSharp(unsigned long data, int nbits);
-  void sendPanasonic(unsigned int address, unsigned long data);
-  void sendJVC(unsigned long data, int nbits, int repeat); // *Note instead of sending the REPEAT constant if you want the JVC repeat signal sent, send the original code value and change the repeat argument from 0 to 1. JVC protocol repeats by skipping the header NOT by sending a separate code value like NEC does.
-  // private:
-  void enableIROut(int khz);
-  VIRTUAL void mark(int usec);
-  VIRTUAL void space(int usec);
-}
-;
-
 // Some useful constants
 
 #define USECPERTICK 50  // microseconds per clock interrupt tick
